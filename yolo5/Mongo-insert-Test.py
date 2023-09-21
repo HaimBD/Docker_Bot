02503e5e6132b270616dd87dd9430a270bc0c809
HaimBD <haimbendavid1995@gmail.com>
2023-09-21 21:35:52 +0300

    Added the ReplicaSet to the MongoFile. 22.09.2023

--- yolo5/Mongo-insert-Test.py
+++ yolo5/Mongo-insert-Test.py
@@ -1,14 +1,14 @@
 import pymongo
 
 prediction_summary = {
-            'Test1': 'Test-One',
-            'Test2': 'Test-Two',
-            'Test3': 'Test-Third',
-            'Test4': 'Test-Four',
-            'Test5': 'Test-Five'
+            'Test1': 'Test-One-One',
+            'Test2': 'Test-Two-One',
+            'Test3': 'Test-Third-One',
+            'Test4': 'Test-Four-One',
+            'Test5': 'Test-Five-One'
         }
 
-mongo_server = pymongo.MongoClient("mongodb://localhost:27017/")
+mongo_server = pymongo.MongoClient("mongodb://mongo1:27017,mongo2:27017,mongo3:27017/?replicaSet=myReplicaSet")
 mongo_database = mongo_server["docker"]
 mongo_collection = mongo_database["Data"]
 mongo_collection.insert_one(prediction_summary)
